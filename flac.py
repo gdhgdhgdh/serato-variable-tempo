928a29916599d7276288ab02e1ae8753445ef1fe
Gavin Hamill <gdh@acentral.co.uk>
2021-03-09 23:38:57 +0000

    wip

--- flac.py
+++ flac.py
@@ -12,47 +12,66 @@ def determine_filetype(inputfile):
 
 def write_flac(flacfile, beatgridfile):
     audio = mutagen.File(flacfile)
+    print(audio)
 
     beatgrid_fp = open(beatgridfile, 'rb')
     beatgrid = beatgrid_fp.read()
     beatgrid_fp.close()
 
-    beatgrid2 = b'application/octet-stream' + b'\x00\x00' + b'Serato BeatGrid' + b'\x00' + beatgrid
-    audio["SERATO_BEATGRID"] = textwrap.fill(base64.b64encode(beatgrid2).decode('ascii'), width=72)
+    beatgrid = b'application/octet-stream' + b'\x00\x00' + b'Serato BeatGrid' + b'\x00' + beatgrid
+    audio["SERATO_BEATGRID"] = textwrap.fill(base64.b64encode(beatgrid).decode('ascii'), width=72)
     audio.save()
 
-def write_mp3(flacfile, beatgridfile):
-    audio = mutagen.File(flacfile)
+def write_mp3(mp3file, beatgridfile):
+    audio = mutagen.id3.ID3(mp3file)
 
     beatgrid_fp = open(beatgridfile, 'rb')
     beatgrid = beatgrid_fp.read()
     beatgrid_fp.close()
 
-    print(audio.info.length)
-    audio["SERATO_BEATGRID"] = base64.b64encode(beatgrid).encode('ascii')
+    audio['GEOB:Serato BeatGrid'] = mutagen.id3.GEOB(
+        encoding=0,
+        mime='application/octet-stream',
+        desc='Serato BeatGrid',
+        data=beatgrid,
+    )
     audio.save()
 
 def gen_beatgrid(inputfile, outputfile):
-    data, rate = librosa.load(inputfile)
+    data, rate = librosa.load(inputfile, sr=None)
     params = {}
+    avg_tempo = librosa.beat.tempo(data)[0]
     beats = vamp.collect(data, rate, "qm-vamp-plugins:qm-barbeattracker")
 
     fp = open(outputfile, 'wb')
 
     fp.write(b'\x01\x00')
-    fp.write(struct.pack('>i', int(len(beats['list']) / 4 + 1))) # only emit a marker on the '1' of each bar
+    print(int(len(beats['list']) / 8 + 1))
+
+    fp.write(struct.pack('>i', int(len(beats['list']) / 8 + 1))) # only emit a marker on the '1' of each bar
 
     lastts = ""
+    process_beat = True
 
-    for item in beats['list']:
-        if item['label'] == '1':
+    for count, item in enumerate(beats['list']):
+        if count == 1016: # serato only allows 128 markers - leave one space for the termination marker
+            break
+#        print(f'pb: {process_beat} item: {item} lastts: {lastts}')
+        if item['label'] == '1' and not process_beat:
+            process_beat = True
+            continue
+        if item['label'] == '1' and process_beat:
+#            print(f"Writing beat at {item['timestamp']}")
             fp.write(struct.pack('>f', item['timestamp']))
-            fp.write(b'\x00\x00\x00\x04')
-            print(item['timestamp'])
+            fp.write(b'\x00\x00\x00\x08')
+#            print(item['timestamp'])
             lastts = item['timestamp']
+            process_beat = False
+            continue
 
+    print(lastts)
     fp.write(struct.pack('>f', lastts))
-    fp.write(struct.pack('>f', 105))
+    fp.write(struct.pack('>f', avg_tempo))
     fp.write(b'\x37')
     print(len(beats['list']))
 
@@ -69,3 +88,7 @@ print(filetype)
 
 if filetype == 'audio/flac':
     write_flac(args.input_file, args.beatgrid_file)
+elif filetype == 'audio/mp3':
+    write_mp3(args.input_file, args.beatgrid_file)
+else:
+    print(f'Sorry, {filetype} files are not supported')
